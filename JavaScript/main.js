8c48679bc3513da9703dbd205576f729a0306966
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-05-19 22:45:00 +0200

    add 3 navbars

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,16 +1,19 @@
+searchform = document.querySelector('.search-form ');
+document.querySelector('#search-btn').onclick = ()=>{
+    searchform.classList.toggle('active');
+}
 
-
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
         if (window.scrollY > 80) {
             document.querySelector('.header .header2').classList.add('fixed-top');
-          // add padding top to show content behind navbar
+            // add padding top to show content behind navbar
             navbar_height = document.querySelector('header').offsetHeight;
             document.body.style.paddingTop = navbar_height + 'px';
         } else {
             document.querySelector('.header .header2').classList.remove('fixed-top');
-           // remove padding top from body
+            // remove padding top from body
             document.body.style.paddingTop = '0';
-        } 
+        }
     });
-    }); 
+});
