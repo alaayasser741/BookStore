cb430d18a585445d028b3b8271ee776b2fde9f42
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-05-20 11:29:30 +0200

    add arrivals  section

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -78,4 +78,24 @@ var swiper = new Swiper(".featured-slider", {
     },
 });
 
-//Next & previous
+//arrivals
+var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
